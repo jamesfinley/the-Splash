affdd412589ca1a21a9843e15ddd41c751300153
jamesfinley <jamesfinley@gmail.com>
2009-08-03 13:12:20 -0500

    Moved jQuery to the admin_resources folder.

--- system/application/views/admin/delete_category.php
+++ system/application/views/admin/delete_category.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>the Splash : Delete Category "<?=$categoryName?>"</title>
-		<script type="text/javascript" src="/resources/javascript/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="/admin_resources/javascript/jquery-1.3.2.min.js"></script>
 		<link rel="stylesheet" href="/admin_resources/css/style.css" />
 	</head>
 	<body>
